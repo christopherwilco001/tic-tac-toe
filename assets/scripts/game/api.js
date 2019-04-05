87fdb4dafa67e73dd4633b5ad59c68d76e38b66d
christopherwilco001 <>
2019-04-05 16:49:11 -0400

    TicTacToe day 3

---
+++ assets/scripts/game/api.js
@@ -0,0 +1,17 @@
+// const config = require('../config')
+// const store = require('../store')
+//
+// // const createNewGame = (formData) => {
+// //   return $.ajax({
+// //     url: config.apiUrl + `games`,
+// //     method: 'POST',
+// //     headers: {
+// //       Authorization: 'Token token=' + store.user.token
+// //     },
+// //     data: formData
+// //   })
+// // }
+
+module.exports = {
+  // createNewGame
+}
--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -3,45 +3,88 @@
 const api = require('./api.js')
 const ui = require('./ui.js')
 
-const gameBoard = ['', '', '', '', '', '', '', '', '']
+const board = ['', '', '', '', '', '', '', '', '']
 
 let playerTurn = 'X'
 
-// const winConditions = [
-//   [0, 1, 2], [3, 4, 5],
-//   [6, 7, 8], [0, 3, 6],
-//   [1, 4, 7], [2, 5, 8],
-//   [0, 4, 8], [6, 4, 2]
-// ]
+
+const winConditions = [
+  [0, 1, 2], [3, 4, 5],
+  [6, 7, 8], [0, 3, 6],
+  [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [6, 4, 2]
+]
 //
-// const createNewGame = function () {
-//   event.preventDefault()
-//   api.createNewGame()
-//     .then(ui.createGameSuccess)
-//     .catch(ui.createGameFailure)
-//   $('#create-game').on('click')
-//   $('.col-4').empty()
-// }
 
 const onClick = function (event) {
   const currentTurn = $(event.target).data('id')
   console.log(currentTurn)
 
-  if (playerTurn === 'X' && $(event.target).text('')) {
+  if (playerTurn === 'X' && $(event.target).text() === '') {
     $(event.target).text('X')
     playerTurn = 'O'
-  } else if (playerTurn === 'O' && $(event.target).text('')) {
+  } else if (playerTurn === 'O' && $(event.target).text() === '') {
     $(event.target).text('O')
     playerTurn = 'X'
   } else {
-
+    alert('Make another move')
   }
 }
 
+const playerWin = [
+  [$('#zero').text(), $('#one').text(), $('#two').text()],
+  // row 1
+  [$('#three').text(), $('#four').text(), $('#five').text()],
+  // row 2
+  [$('#six').text(), $('#seven').text(), $('#eight').text()],
+  // row 3
+  [$('#zero').text(), $('#three').text(), $('#six').text()],
+  // col 1
+  [$('#one').text(), $('#four').text(), $('#seven').text()],
+  // col 2
+  [$('#two').text(), $('#five').text(), $('#eight').text()],
+  // col 3
+  [$('#zero').text(), $('#four').text(), $('#eight').text()],
+  // diag 1
+  [$('#two').text(), $('#four').text(), $('#six').text()]
+  // diag 2
+]
+
+// for (let i = 0; i < board.length; i++) {
+//   if (board[i]) {
+//   }
+//   // console.log('Win')
+// }
+
+// if (board[0] === board[1] && board[1] === board[2] === 'X')
+
+
+
+// if (playerWin === true) {
+//   console.log('current player wins!')
+//
+//
+
+// const createNewGame = function (event) {
+//   event.preventDefault()
+//   api.createNewGame()
+//     .then(ui.createGameSuccess)
+//     .catch(ui.createGameFailure)
+//
+//   // $('.col-4').empty()
+// }
+
+
+
 const addHandlers = function () {
   $('.box').on('click', onClick)
+  // $('')
+  // $('#create-game').on('click', createGameSuccess)
 }
 
 module.exports = {
-  addHandlers
+  addHandlers,
+  winConditions,
+  board,
+  playerWin
 }
